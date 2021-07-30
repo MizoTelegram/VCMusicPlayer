bd9d18c90222adaf051594a082ff624796764614
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-07-30 13:12:11 +0530

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,178 @@
+#MIT License
+
+#Copyright (c) 2021 Zaute Km
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import Client, emoji
+from utils import mp
+from config import Config
+playlist=Config.playlist
+
+HELP = """
+*User Commands:**
+▷/play **[hla hming]/[yt link]**: Audio file Reply Rawh!
+▷/dplay **[hla hming]:** Deezer Music aṭanga play na.
+▷/player: Hla play mek enna.
+▷/help: Min Commands theihna tur.
+▷/playlist: Playlists enna.
+**Admin Commands:**
+▷/skip **[n]** ...  Skip current or n where n >= 2
+▷/join: Join voice chat.
+▷/leave: Leave current voice chat
+▷/vc: Check which VC is joined.
+▷/stop: Stop playing.
+▷/radio: Start Radio.
+▷/stopradio: Stops Radio Stream.
+▷/replay: Play from the beginning.
+▷/clean: Remove unused RAW PCM files.
+▷/pause: Pause playing.
+▷/resume: Resume playing.
+▷/volume: Change volume(0-200).
+▷/mute: Mute in VC.
+▷/unmute: Unmute in VC.
+▷/restart: Restarts the Bot.
+"""
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.from_user.id not in Config.ADMINS and query.data != "help":
+        await query.answer(
+            "Who the hell you are",
+            show_alert=True
+            )
+        return
+    else:
+        await query.answer()
+    if query.data == "replay":
+        group_call = mp.group_call
+        if not playlist:
+            return
+        group_call.restart_playout()
+        if not playlist:
+            pl = f"{emoji.NO_ENTRY} Empty Playlist"
+        else:
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        await query.edit_message_text(
+                f"{pl}",
+                parse_mode="Markdown",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔄", callback_data="replay"),
+                            InlineKeyboardButton("⏯", callback_data="pause"),
+                            InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                        ],
+                    ]
+                )
+            )
+
+    elif query.data == "pause":
+        if not playlist:
+            return
+        else:
+            mp.group_call.pause_playout()
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Paused\n\n{pl}",
+        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔄", callback_data="replay"),
+                            InlineKeyboardButton("⏯", callback_data="resume"),
+                            InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                        ],
+                    ]
+                )
+            )
+
+    
+    elif query.data == "resume":   
+        if not playlist:
+            return
+        else:
+            mp.group_call.resume_playout()
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Resumed\n\n{pl}",
+        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔄", callback_data="replay"),
+                            InlineKeyboardButton("⏯", callback_data="pause"),
+                            InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                        ],
+                    ]
+                )
+            )
+
+    elif query.data=="skip":   
+        if not playlist:
+            return
+        else:
+            await mp.skip_current_playing()
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        try:
+            await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Skipped\n\n{pl}",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔄", callback_data="replay"),
+                        InlineKeyboardButton("⏯", callback_data="pause"),
+                        InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                    ],
+                ]
+            )
+        )
+        except:
+            pass
+    elif query.data=="help":
+        buttons = [
+            [
+                InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/ZauteKm/tgvc-RadioBot'),
+            ],
+            [
+               InlineKeyboardButton('👥 Group', url='https://t.me/mizotelegram/106'),
+               InlineKeyboardButton('Channel 📢', url='https://t.me/joinchat/0io4YMY-xms1M2Q9'),
+            ],
+            [
+               InlineKeyboardButton('🛡️Nangma pualin i siam thei🛡️', url='https://t.me/c/1481808444/131'),
+        
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.edit_message_text(
+            HELP,
+            reply_markup=reply_markup
+
+        )
+
